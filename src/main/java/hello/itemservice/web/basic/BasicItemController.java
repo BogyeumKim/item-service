8a5cb7782af0708133d96159110e8b0b3c5ab5fa
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-03-21 17:47:39 +0900

    PRG 패턴
    POST - Redirect - GET 방식으로
    POST를 보낸상태에서 새로고침하면 계속 POST가 보내지는걸 방지하기위해 Redirect로 GET 주소로 이동시켜준다.
    
    단점은 뒤쪽에 + item.getId() 이런식으로 직접넣으면 위험함

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -72,13 +72,20 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(Item item) {
 
         /* String,int 같은 단순타입 이면 @RequestParam , 아니면 @ModelAttribute 알아서 붙음  */
 
         itemRepository.save(item);
         return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
+
     }
 
 
